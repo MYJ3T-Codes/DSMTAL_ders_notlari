5e5af348bfb401cf47012386fe0f39a360f5c74a
MYJ3T-Codes <118949828+MYJ3T-Codes@users.noreply.github.com>
2023-12-15 00:52:19 +0300

    dersteki for soruları ve cevaplar vs

---
+++ for.py
@@ -0,0 +1,59 @@
+# 1 den 20ye kadar 3 e bölünen sayıları yazıdırma
+'''
+for i in range(1,20):
+    if (i/3) == 0:
+        print(i)
+'''
+'''
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+yazdıran prgram
+'''
+'''
+#excel tablosu
+for i in range(6):
+    print()
+    for k in range(6):
+        print(k,end="  ")
+#çarpım tablosu
+'''
+#10 a kadar çarpan tablosu
+'''
+for i in range(1,10):
+    print()
+    for k in range(1,9):
+        print(i*k,end="\t")
+'''
+'''
+klavyeden girilen uzun kenar ve kısa kenar sayılarına göre * işaretlerinden oluşan bir diktörgen girin
+'''
+'''
+uzun_kenar = int(input("uzun kenarı giriniz:"))
+kisa_kenar = int(input("kısa kenarı giriniz:"))
+for i in range(kisa_kenar):
+    print("*"*uzun_kenar)
+    print()
+    for k in range(uzun_kenar):
+        print("*",end="")
+    print()
+'''
+#üçgen yapımı
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
